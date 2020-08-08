cbe91b2170488b6e5a0c7e6c14846d2abe70cf3a
Ricardo Herrera MAC <rherrerapetit@gmail.com>
2020-08-07 23:48:42 -0500

    Initial Commit

---
+++ storyFeed/storyFeed.h
@@ -0,0 +1,19 @@
+//
+//  storyFeed.h
+//  storyFeed
+//
+//  Created by Ricardo Herrera Petit on 8/7/20.
+//  Copyright © 2020 Ricardo Herrera Petit. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for storyFeed.
+FOUNDATION_EXPORT double storyFeedVersionNumber;
+
+//! Project version string for storyFeed.
+FOUNDATION_EXPORT const unsigned char storyFeedVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <storyFeed/PublicHeader.h>
+
+
