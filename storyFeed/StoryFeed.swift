64801e8811d487c7ecb7d254663a756625f30f3f
Ricardo Herrera MAC <rherrerapetit@gmail.com>
2020-08-07 23:58:18 -0500

    Changed testable import to import and created a public initializer for the StoryFeedClass

--- storyFeed/StoryFeed.swift
+++ storyFeed/StoryFeed.swift
@@ -1,14 +1,9 @@
-//
-//  StoryFeed.swift
-//  storyFeed
-//
-//  Created by Ricardo Herrera Petit on 8/7/20.
-//  Copyright © 2020 Ricardo Herrera Petit. All rights reserved.
-//
-
 import Foundation
 
 
 public class StoryFeed {
-    var numberOfStories = 0
+    public var numberOfStories:Int!
+    public init() {
+        self.numberOfStories = 0
+    }
 }
--- storyFeedTests/storyFeedTests.swift
+++ storyFeedTests/storyFeedTests.swift
@@ -1,13 +1,5 @@
-//
-//  storyFeedTests.swift
-//  storyFeedTests
-//
-//  Created by Ricardo Herrera Petit on 8/7/20.
-//  Copyright © 2020 Ricardo Herrera Petit. All rights reserved.
-//
-
 import XCTest
-@testable import storyFeed
+import storyFeed
 
 
 class storyFeedTests: XCTestCase {
