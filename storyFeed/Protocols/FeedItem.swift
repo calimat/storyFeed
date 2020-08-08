c1a5f614b4215115e8182e78ab100d024f764304
Ricardo Herrera MAC <rherrerapetit@gmail.com>
2020-08-08 15:15:13 -0500

    Changed to FeedItem to conform to the Liskov Substitution principle.

--- storyFeed/Protocols/FeedItem.swift
+++ storyFeed/Protocols/FeedItem.swift
@@ -1,6 +1,9 @@
 import Foundation
 
 public protocol FeedItem {
+    var id:UUID { get}
+    var addedDate:Date? { get }
+    var expirationDate:Date? { get }
     func setAddeddate(date:Date?)
     func setExpirationDate(date:Date?)
 }
